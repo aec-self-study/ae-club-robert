165a0fd4db667b70d0c8e67932ddb0a52de3e538
rschembri <20638107+rschembri@users.noreply.github.com>
2023-07-20 12:16:54 +0000

    person a made some changes

--- customers.sql
+++ customers.sql
@@ -5,7 +5,7 @@ C.id
 ,C.email 
 ,MIN(O.created_at) as first_order_at
 ,COUNT(O.id) as number_of_orders
-
+--changes by Person A in my-newer-branch
 from `analytics-engineers-club.coffee_shop.customers` as C
 left join `analytics-engineers-club.coffee_shop.orders` as O on C.id = O.customer_id
 
