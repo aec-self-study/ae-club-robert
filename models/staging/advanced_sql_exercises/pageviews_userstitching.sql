0de7a61f6e5030d5be3bd3715e16bcbcc374624d
rschembri <20638107+rschembri@users.noreply.github.com>
2023-08-02 09:27:03 +0000

    User Stitching

--- models/staging/advanced_sql_exercises/pageviews_userstitching.sql
+++ models/staging/advanced_sql_exercises/pageviews_userstitching.sql
@@ -2,7 +2,7 @@
     materialized= 'table'
 )}}
 
-SELECT * from {{ source('web_tracking', 'pageviews')}}
+-- SELECT * from {{ source('web_tracking', 'pageviews')}}
 
 --id,
 --visitor_id,		
@@ -12,3 +12,29 @@ SELECT * from {{ source('web_tracking', 'pageviews')}}
 --customer_id
 
 --from `analytics-engineers-club.web_tracking.pageviews`
+
+with pageviews as (
+    select * from {{ source('web_tracking', 'pageviews') }}
+),
+
+visitors as (
+    select * from {{ ref('visitors_userstitching') }}
+),
+
+joined as (
+    select
+        pageviews.*,
+        -- customer id if we have it, else visitor id
+        coalesce(
+            visitors.first_customer_id,
+            pageviews.visitor_id
+        ) as blended_user_id,
+
+        visitors.first_visitor_id
+
+    from pageviews
+    left join visitors
+        on pageviews.visitor_id = visitors.visitor_id
+)
+
+select * from joined
--- target/run/dbt_robert/models/staging/advanced_sql_exercises/pageviews_userstitching.sql
+++ target/run/dbt_robert/models/staging/advanced_sql_exercises/pageviews_userstitching.sql
@@ -10,7 +10,7 @@
     as (
       
 
-SELECT * from `analytics-engineers-club`.`web_tracking`.`pageviews`
+-- SELECT * from `analytics-engineers-club`.`web_tracking`.`pageviews`
 
 --id,
 --visitor_id,		
@@ -20,5 +20,31 @@ SELECT * from `analytics-engineers-club`.`web_tracking`.`pageviews`
 --customer_id
 
 --from `analytics-engineers-club.web_tracking.pageviews`
+
+with pageviews as (
+    select * from `analytics-engineers-club`.`web_tracking`.`pageviews`
+),
+
+visitors as (
+    select * from `aec-students`.`dbt_robert`.`visitors_userstitching`
+),
+
+joined as (
+    select
+        pageviews.*,
+        -- customer id if we have it, else visitor id
+        coalesce(
+            visitors.first_customer_id,
+            pageviews.visitor_id
+        ) as blended_user_id,
+
+        visitors.first_visitor_id
+
+    from pageviews
+    left join visitors
+        on pageviews.visitor_id = visitors.visitor_id
+)
+
+select * from joined
     );
   
