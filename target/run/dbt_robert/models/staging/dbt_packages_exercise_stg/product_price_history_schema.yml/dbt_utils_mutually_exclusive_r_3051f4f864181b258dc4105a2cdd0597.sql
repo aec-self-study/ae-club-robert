169da655d73c4fc96689372b07aad1b7e822502e
rschembri <20638107+rschembri@users.noreply.github.com>
2023-08-03 12:15:26 +0000

    mutually_exclusive_ranges test added

---
+++ target/run/dbt_robert/models/staging/dbt_packages_exercise_stg/product_price_history_schema.yml/dbt_utils_mutually_exclusive_r_3051f4f864181b258dc4105a2cdd0597.sql
@@ -0,0 +1,83 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+
+    
+    
+
+
+    
+    
+
+
+
+
+with window_functions as (
+
+    select
+        
+        product_id as partition_by_col,
+        
+        created_at as lower_bound,
+        ended_at as upper_bound,
+
+        lead(created_at) over (
+            partition by product_id
+            order by created_at, ended_at
+        ) as next_lower_bound,
+
+        row_number() over (
+            partition by product_id
+            order by created_at desc, ended_at desc
+        ) = 1 as is_last_record
+
+    from `aec-students`.`dbt_robert`.`product_price_history`
+
+),
+
+calc as (
+    -- We want to return records where one of our assumptions fails, so we'll use
+    -- the `not` function with `and` statements so we can write our assumptions more cleanly
+    select
+        *,
+
+        -- For each record: lower_bound should be < upper_bound.
+        -- Coalesce it to return an error on the null case (implicit assumption
+        -- these columns are not_null)
+        coalesce(
+            lower_bound < upper_bound,
+            false
+        ) as lower_bound_less_than_upper_bound,
+
+        -- For each record: upper_bound < the next lower_bound.
+        -- Coalesce it to handle null cases for the last record.
+        coalesce(
+            upper_bound < next_lower_bound,
+            is_last_record,
+            false
+        ) as upper_bound_less_than_next_lower_bound
+
+    from window_functions
+
+),
+
+validation_errors as (
+
+    select
+        *
+    from calc
+
+    where not(
+        -- THE FOLLOWING SHOULD BE TRUE --
+        lower_bound_less_than_upper_bound
+        and upper_bound_less_than_next_lower_bound
+    )
+)
+
+select * from validation_errors
+
+      
+    ) dbt_internal_test
