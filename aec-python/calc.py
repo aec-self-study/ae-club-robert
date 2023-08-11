0e409ad11905501072502bcc4877520d07459ada
rschembri <20638107+rschembri@users.noreply.github.com>
2023-08-11 13:35:18 +0000

    CLI

---
+++ aec-python/calc.py
@@ -0,0 +1,23 @@
+# calc.py
+
+import argparse
+
+parser = argparse.ArgumentParser(description= "CLI Calculator")
+
+subparsers = parser.add_subparsers(help = "sub-command help", dest="command")
+
+add = subparsers.add_parser("add", help = "add integers")
+add.add_argument("ints_to_sum", nargs=2 , type=int)
+
+sub = subparsers.add_parser("sub", help = "subtract integers")
+sub.add_argument("ints_to_sub", nargs=2 , type=int)
+
+args = parser.parse_args()
+
+if args.command =="add":
+    our_sum = sum(args.ints_to_sum)
+    print(f"the sum of values is:  {our_sum}")
+
+if args.command =="sub":
+    our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
+    print(f"the subtraction of values is:  {our_sub}")
