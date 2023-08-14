8bbdaed28f8a93fe5f321868a5471ced593f250d
rschembri <20638107+rschembri@users.noreply.github.com>
2023-08-14 07:08:33 +0000

    python exercise

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -7,10 +7,16 @@ parser = argparse.ArgumentParser(description= "CLI Calculator")
 subparsers = parser.add_subparsers(help = "sub-command help", dest="command")
 
 add = subparsers.add_parser("add", help = "add integers")
-add.add_argument("ints_to_sum", nargs=2 , type=int)
+add.add_argument("ints_to_sum", nargs='*' , type=int)
 
 sub = subparsers.add_parser("sub", help = "subtract integers")
 sub.add_argument("ints_to_sub", nargs=2 , type=int)
+
+div = subparsers.add_parser("div", help = "divide integers")
+div.add_argument("ints_to_div", nargs=2 , type=int)
+
+mult = subparsers.add_parser("mult", help = "multiply integers")
+mult.add_argument("ints_to_mult", nargs=2 , type=int)
 
 args = parser.parse_args()
 
@@ -21,3 +27,11 @@ if args.command =="add":
 if args.command =="sub":
     our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
     print(f"the subtraction of values is:  {our_sub}")
+
+if args.command =="div":
+    our_div = args.ints_to_div[0] / args.ints_to_div[1]
+    print(f"the result of dividing values is:  {our_div}")
+
+if args.command =="mult":
+    our_mult = args.ints_to_mult[0] * args.ints_to_mult[1]
+    print(f"the result of multiplying values is:  {our_mult}")
